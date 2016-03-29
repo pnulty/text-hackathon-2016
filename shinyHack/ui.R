db1bbbd0373eefcf24756f844623963c9b2ca45c
Paul <paul.nulty@gmail.com>
2016-03-30 00:35:17 +0100

    link to dataset

--- shinyHack/ui.R
+++ shinyHack/ui.R
@@ -1,15 +1,18 @@
 
 shinyUI(fluidPage(
-    headerPanel("Live text prediction"),
+    headerPanel(h6("Live text prediction")),
     sidebarPanel(
         tags$textarea(id = 'speech', placeholder = 'Type here', rows = 8,style="width:800px;", ""), 
+        h6('Live text category prediction using elastic net regression trained on 2016 US Presidential candidate 
+           debates. Hackathon Entry by Paul Nulty, for Visual Text Analytics Workshop 2016 at LSE.',
+           a(href="https://github.com/pnulty/text-hackathon-2016", "Code and slides. \n"),
+           a(href="http://www.textasdata.com/2016/03/visualization-workshop-hackathon-challenge/",
+             "Dataset for challenge")),
         width=12
     ),
     mainPanel(
         fluidRow(
             splitLayout(cellWidths = c("80%", "30%"), plotOutput("mainBar"), plotOutput("partyBar"))
-        ),
-        h6('Hackathon Entry by Paul Nulty, for Visual Text Analytics Workshop 2016 at LSE.',
-           a(href="https://github.com/pnulty/text-hackathon-2016", "Code and slides here."))
+        )
     )
 ))
